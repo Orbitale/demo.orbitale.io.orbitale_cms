0b0cedaa9874ef2539677392fbfa6dfa2fc7b8eb
Alex Rock Ancelet <pierstoval@gmail.com>
2023-02-03 20:32:16 +0100

    Fix prod issue

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 use App\Kernel;
 
+$_SERVER['REQUEST_URI'] = preg_replace('~^/orbitale_cms~', '', $_SERVER['REQUEST_URI']);
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
