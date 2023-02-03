da3fe4b21ae05c5e005e06d735c474c8b8ea47ef
Alex Rock Ancelet <pierstoval@gmail.com>
2023-02-03 21:22:35 +0100

    Fix proxy issues

--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Kernel;
-
-$_SERVER['REQUEST_URI'] = preg_replace('~^/orbitale_cms~', '', $_SERVER['REQUEST_URI']);
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    $_SERVER['REQUEST_URI'] = preg_replace('~^/orbitale_cms~', '', $_SERVER['REQUEST_URI']);
+    Request::setTrustedProxies([$_SERVER['REMOTE_ADDR']], Request::HEADER_X_FORWARDED_PREFIX);
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
